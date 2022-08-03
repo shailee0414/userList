0421f12f2f1a1465bf6cd990abcb1f7820ecdf56
Shailee Yadav <shaileeyadav130@gmail.com>
2022-08-03 17:39:15 +0530

    form modification

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './Login.css'
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -29,7 +28,7 @@ const Login = () => {
     }
     
     return (
-        <div className="s" style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '6rem' ,   marginLeft:'7rem',   marginRight:'7rem'}}>
+        <div className="s" style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '6rem' ,   marginLeft:'4rem',   marginRight:'4rem'}}>
             <div className="login-form" style={{ border: "solid 1px grey", borderRadius: '10px', padding: '20px', width: '40rem' }}>
                 <form className="ui form">
                     <div className="field">
